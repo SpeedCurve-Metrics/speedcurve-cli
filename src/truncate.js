1ad0008be5b02d8c69d217fb70c78ab6b6b44601
Joseph Wynn <joseph@wildlyinaccurate.com>
2018-12-20 08:55:22 +1300

    Fix truncate() ellipsis

--- src/truncate.js
+++ src/truncate.js
@@ -1,7 +1,7 @@
 module.exports = function truncate(str, length) {
   const sliced = str.slice(0, length)
 
-  if (sliced.length < str.length + 3) {
+  if (sliced.length + 3 < str.length) {
     return sliced + "..."
   }
 

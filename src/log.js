2b915cb9eaded1eee23c7842f9e54cf33ea40a79
Joseph Wynn <joseph@speedcurve.com>
2019-05-21 18:28:40 +1200

    Tweak log output to distinguish between errors and "bad" messages

--- src/log.js
+++ src/log.js
@@ -2,12 +2,13 @@ const log = require("npmlog")
 
 log.level = "silent"
 log.prefixStyle = {}
-log.addLevel("ok", 5000, { fg: "green", bg: "black" }, "OK")
+log.addLevel("ok", 5000, { fg: "green" }, "✔")
+log.addLevel("bad", 5000, { fg: "red" }, "✖")
 
 // In the interests of using a single interface for writing output, this sets up
 // a "stdout" log level which is never written to stderr by npmlog but is instead
 // handled here and passed directly to process.stdout
-log.addLevel("stdout", Infinity, { fg: "green", bg: "black" })
+log.addLevel("stdout", Infinity, { fg: "green" })
 log.on("log.stdout", log => {
   process.stdout.write(log.prefix + log.message)
 })

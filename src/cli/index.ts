44cffea1c12c71279cc884bb683876e7508c4b91
Joseph Wynn <joseph@speedcurve.com>
2019-06-24 09:28:47 +1200

    Explicitly import URL for Node<10

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import yargs from "yargs"
+import { URL } from "url"
 import { api } from "../api"
 import { Command } from "../command"
 import budgetsCommand from "../command/budgets"

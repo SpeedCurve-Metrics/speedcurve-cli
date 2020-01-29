9ad565d04d6eb535989dbf53dee3fb81ae96b075
Joseph Wynn <joseph@speedcurve.com>
2020-01-29 19:24:17 +1300

    Add tests for urls module

---
+++ __tests__/urls.test.js
@@ -0,0 +1,33 @@
+const SpeedCurve = require("../dist")
+const request = require("request-promise-native")
+const SPEEDCURVE_API_KEY = "abc123"
+
+test("SpeedCurve.urls.create()", async () => {
+	request.post.mockResolvedValueOnce({
+		status: "success",
+		message: "Success!",
+		url_id: 8081
+	})
+
+	const response = await SpeedCurve.urls.create(SPEEDCURVE_API_KEY, {
+		siteId: 326,
+		url: "https://speedcurve.com/",
+		label: "Home"
+	})
+
+	expect(response.status).toEqual("success")
+	expect(request.post.mock.calls[0][0].form.site_id).toEqual(326)
+})
+
+test("SpeedCurve.urls.update()", async () => {
+	request.put.mockResolvedValueOnce({
+		status: "success",
+		message: "Success!",
+		url_id: 8081
+	})
+
+	const response = await SpeedCurve.urls.update(SPEEDCURVE_API_KEY, 8081, { label: "home" })
+
+	expect(response.status).toEqual("success")
+	expect(request.put.mock.calls[0][0].form.label).toEqual("home")
+})
